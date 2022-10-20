d64b3b87a88a93ff499d32f4de62d62726cef229
Fut <shmopls@gmail.com>
2022-10-20 14:36:46 +0500

    PostController.createPost. Создание вакансии

--- src/main/java/dreamjob/controller/PostController.java
+++ src/main/java/dreamjob/controller/PostController.java
@@ -6,6 +6,8 @@ import dreamjob.store.PostStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
 
@@ -32,5 +34,30 @@ public class PostController {
         model.addAttribute("candidate",
                 new Candidate(0, "Введите имя кандидата", "Заполните описание", LocalDateTime.now()));
         return "addCandidate";
+    }
+
+     /**
+     * Метод для обработки HttpRequest`а мануально с созданием нового экземпляра Post
+     * @param страница с вакансиями
+     * @return возврат на страницу с вакансиями
+    @PostMapping("/createPost")
+    public String createPost(HttpServletRequest req) {
+        String name = req.getParameter("name");
+        System.out.println(name);
+        postStore.add(new Post(1, name));
+        return "redirect:/posts";
+    }
+     */
+
+    /**
+     * Метод для обработки HttpRequest`а силами Spring с автоматическим созданием нового экземпляра Post
+     * TODO: реализовать генерацию id (по согласованию с ментором)
+     * @param post
+     * @return
+     */
+    @PostMapping("/createPost")
+    public String createPost(@ModelAttribute Post post) {
+        postStore.add(post);
+        return "redirect:/posts";
     }
 }
