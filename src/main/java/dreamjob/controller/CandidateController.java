7d9b3de66ff5867280ad6c44de0b5327d52c199a
Fut <shmopls@gmail.com>
2022-10-20 17:01:24 +0500

    Создание и редактирование кандидатов

--- src/main/java/dreamjob/controller/CandidateController.java
+++ src/main/java/dreamjob/controller/CandidateController.java
@@ -1,9 +1,14 @@
 package dreamjob.controller;
 
+import dreamjob.model.Candidate;
+import dreamjob.model.Post;
 import dreamjob.store.CandidateStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CandidateController {
@@ -14,5 +19,23 @@ public class CandidateController {
     public String candidates(Model model) {
         model.addAttribute("candidates", candidateController.findAll());
         return "candidates";
+    }
+
+    @PostMapping("/createCandidate")
+    public String createCandidate(@ModelAttribute Candidate candidate) {
+        candidateController.add(candidate);
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/formUpdateCandidates/{candidateId}")
+    public String formUpdateCandidate(Model model, @PathVariable("candidateId") int id) {
+        model.addAttribute("candidates", candidateController.findById(id));
+        return "updateCandidate";
+    }
+
+    @PostMapping("/updateCandidate")
+    public String updateCandidate(@ModelAttribute Candidate candidate) {
+        candidateController.update(candidate);
+        return "redirect:/candidates";
     }
 }
