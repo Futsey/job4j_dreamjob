8b14cf9e057527386b244b1e62a812ce67f7df54
Fut <shmopls@gmail.com>
2022-11-09 09:22:31 +0500

    Формы. Загрузка файла на сервер

--- src/main/java/dreamjob/model/Post.java
+++ src/main/java/dreamjob/model/Post.java
@@ -33,6 +33,14 @@ public class Post implements Serializable {
         this.created = created;
     }
 
+    public Post(int id, String name, String description, LocalDateTime created, City city) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+        this.city = city;
+    }
+
     public int getId() {
         return id;
     }
--- src/main/java/dreamjob/store/PostStore.java
+++ src/main/java/dreamjob/store/PostStore.java
@@ -1,5 +1,6 @@
 package dreamjob.store;
 
+import dreamjob.model.City;
 import dreamjob.model.Post;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
@@ -18,9 +19,9 @@ public class PostStore {
     private final AtomicInteger idGen = new AtomicInteger(3);
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "Weak Programmer", LocalDateTime.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "Average Programmer", LocalDateTime.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "Strong Programmer", LocalDateTime.now()));
+        posts.put(1, new Post(1, "Junior Java Job", "Weak Programmer", LocalDateTime.now(), new City(4, "Омск")));
+        posts.put(2, new Post(2, "Middle Java Job", "Average Programmer", LocalDateTime.now(), new City(5, "Новосибирск")));
+        posts.put(3, new Post(3, "Senior Java Job", "Strong Programmer", LocalDateTime.now(), new City(6, "Тюмень")));
     }
 
     public Collection<Post> findAll() {
